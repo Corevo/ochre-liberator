b8b05cc356eecb6f793a0bbde4ac568717f655b1
Tomer <gmtomer@gmail.com>
2015-11-11 10:33:53 +0200

    should work

---
+++ index.js
@@ -0,0 +1,9 @@
+var exec = require('child_process').exec;
+
+function getFileContents(path, cb) {
+    exec('libreoffice5.0 --headless --cat ' + path, function (error, stdout, stderr) {
+        cb(error, stdout);
+    });
+}
+
+module.exports.getFileContents = getFileContents;
